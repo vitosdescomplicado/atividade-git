5553d2a298f16eb5583d0ba39f6368c702ae7852
vitosnatios <vitosdeveloper@gmail.com>
2024-02-08 21:24:32 -0300

    pudim card

---
+++ script.js
@@ -0,0 +1,7 @@
+const comer = document.querySelector('#comer-btn');
+const pudimImg = document.querySelector('#pudim-img');
+
+if (comer && pudimImg && pudimImg instanceof HTMLImageElement) {
+  const handleClick = () => (pudimImg.src = '/public/prato.png');
+  comer.addEventListener('click', handleClick);
+}
